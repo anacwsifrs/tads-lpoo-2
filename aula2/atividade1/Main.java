daa917773c7743761e57a18dc6f12a7060499e77
Ana Cláudia Würch Seibert <2025000434@aluno.canoas.ifrs.edu.br>
2026-08-03 19:33:13 -0300

    Exercicio 1 aula 2

--- aula2/atividade1/Main.java
+++ aula2/atividade1/Main.java
@@ -1,27 +1,21 @@
-import java.time.LocalDate;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Produto> produtos = new ArrayList<>();
 
-        produtos.add(new Produto("Caderno", 25.00, 30));
-        produtos.add(new ProdutoPerecivel("Leite", 7.90, 20, LocalDate.of(2026, 8, 20)));
-        produtos.add(new ProdutoNaoPerecivel("Teclado", 150.00, 15, 12));
+        Scanner scanner = new Scanner(System.in);
+        Menu menu = new Menu();
+        opc = scanner.nextInt();
 
-        System.out.println("=== LISTAGEM DE ESTOQUE (POLIMORFISMO) ===");
-        for (Produto p : produtos) {
-            p.mostrarEstoque();
-            System.out.println("------------------------------------");
+        while(opc !=5){
+            menu.exibirMenu();
+            switch(opc){
+                case 1:
+                    System.out.println("Escreva o nome do produto: ");
+            }
         }
 
-        System.out.println("\n=== TESTANDO OPERAÇÕES ===");
-        produtos.get(1).vender(5);
-        produtos.get(2).repor(5);
-
-        System.out.println("\n=== ESTOQUE ATUALIZADO DO LEITE E TECLADO ===");
-        produtos.get(1).mostrarEstoque();
-        System.out.println("------------------------------------");
-        produtos.get(2).mostrarEstoque();
     }
 }
