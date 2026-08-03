ac3a505d545cd8b208f502ecbd8ffe6fa78c1c02
Ana Cláudia Würch Seibert <2025000434@aluno.canoas.ifrs.edu.br>
2026-08-03 19:48:41 -0300

    Exercicio 1 aula 2

--- aula2/atividade1/Main.java
+++ aula2/atividade1/Main.java
@@ -4,9 +4,13 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Produto> produtos = new ArrayList<>();
-        int opc = 0;
         Scanner scanner = new Scanner(System.in);
         Menu menu = new Menu();
+
+        int opc = 0;
+        String nomeProduto;
+        double precoProduto;
+        int quantidadeProduto;
 
         while(opc !=5){
             menu.exibirMenu();
@@ -14,9 +18,21 @@ public class Main {
             switch(opc){
                 case 1:
                     System.out.println("Escreva o nome do produto: ");
+                    nomeProduto = scanner.nextLine();
+
+                    System.out.println("Escreva o preço do produto: ");
+                    precoProduto = scanner.nextDouble();
+
+                    System.out.println("Escreva a quantidade do produto: ");
+                    quantidadeProduto = scanner.nextInt();
+
+                    produtos.add(new Produto(nomeProduto, precoProduto, quantidadeProduto));
+
                     break;
                 case 2:
-                    System.out.println("Escreva o nome do produto: ");
+                    for (Produto produto : produtos) {
+                        produto.mostrarEstoque();
+                    }
                     break;
                 case 3:
                     System.out.println("Escreva o nome do produto: ");
--- aula2/atividade1/Menu.java
+++ aula2/atividade1/Menu.java
@@ -1,9 +1,9 @@
 public class Menu {
     public void exibirMenu() {
         System.out.println("1. Cadastrar Produto");
-        System.out.println("2. Repor Produto");
+        System.out.println("2. Listar Produtos");
         System.out.println("3. Vender Produto");
-        System.out.println("4. Listar Produto");
+        System.out.println("4. Repor Produto");
         System.out.println("5. Sair");
     }
 }
