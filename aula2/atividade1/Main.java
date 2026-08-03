1001afbf74fb7f6c2e6ee5ea2a0dcf4de27bd595
Ana Cláudia Würch Seibert <2025000434@aluno.canoas.ifrs.edu.br>
2026-08-03 19:37:16 -0300

    Exercicio 1 aula 2

--- aula2/atividade1/Main.java
+++ aula2/atividade1/Main.java
@@ -4,16 +4,26 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Produto> produtos = new ArrayList<>();
-
+        int opc = 0;
         Scanner scanner = new Scanner(System.in);
         Menu menu = new Menu();
-        opc = scanner.nextInt();
 
         while(opc !=5){
             menu.exibirMenu();
+            opc = scanner.nextInt();
             switch(opc){
                 case 1:
                     System.out.println("Escreva o nome do produto: ");
+                    break;
+                case 2:
+                    System.out.println("Escreva o nome do produto: ");
+                    break;
+                case 3:
+                    System.out.println("Escreva o nome do produto: ");
+                    break;
+                case 4:
+                    System.out.println("Escreva o nome do produto: ");
+                    break;
             }
         }
 
